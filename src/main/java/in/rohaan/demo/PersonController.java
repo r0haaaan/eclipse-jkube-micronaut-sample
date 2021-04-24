c67f61ed31bfe15c791f8888382baad0a9f100d7
Rohan Kumar <rohaan@redhat.com>
2021-04-24 20:07:27 +0530

    Initial Commit

---
+++ src/main/java/in/rohaan/demo/PersonController.java
@@ -0,0 +1,56 @@
+package in.rohaan.demo;
+
+import in.rohaan.demo.model.Person;
+import io.micronaut.core.annotation.Nullable;
+import io.micronaut.core.version.annotation.Version;
+import io.micronaut.http.annotation.Body;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.validation.Validated;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Controller("/persons")
+@Validated
+public class PersonController {
+
+    List<Person> persons = new ArrayList<>();
+
+    @Post
+    public Person add(@Body @Valid Person person) {
+        person.setId(persons.size() + 1);
+        persons.add(person);
+        return person;
+    }
+
+    @Get("/{id:4}")
+    public Optional<Person> findById(@NotNull Integer id) {
+        return persons.stream()
+                .filter(it -> it.getId().equals(id))
+                .findFirst();
+    }
+
+    @Version("1")
+    @Get("{?max,offset}")
+    public List<Person> findAll(@Nullable Integer max, @Nullable Integer offset) {
+        return persons.stream()
+                .skip(offset == null ? 0 : offset)
+                .limit(max == null ? 10000 : max)
+                .collect(Collectors.toList());
+    }
+
+    @Version("2")
+    @Get("?max,offset")
+    public List<Person> findAllV2(@NotNull Integer max, @NotNull Integer offset) {
+        return persons.stream()
+                .skip(offset == null ? 0 : offset)
+                .limit(max == null ? 10000 : max)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/in/rohaan/demo/EclipseJkubeMicronautSampleTest.java
@@ -0,0 +1,21 @@
+package in.rohaan.demo;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class EclipseJkubeMicronautSampleTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
